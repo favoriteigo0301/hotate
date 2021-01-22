cce1a6e7734bb06ad1c5ba04d073c9532ec1ecea
favoriteigo0301 <smalllogical@gmail.com>
2021-01-22 16:58:05 +0900

    #19対応

--- src/main/resources/db/migration/V1__create_sample.sql
+++ src/main/resources/db/migration/V1__create_sample.sql
@@ -1,6 +1,22 @@
-create table sample_memo (
-  id serial primary key not null, 
-  subject varchar(40) not null, 
-  memo text not null, 
-  image blob  
+create table users
+(
+    id         int primary key not null auto_increment,
+    name       varchar(40)        not null,
+    password   varchar(20)        not null,
+    created_at timestamp,
+    updated_at timestamp
 );
+
+create table sample_memo
+(
+    id        int primary key not null auto_increment,
+    user_id   int not null,
+    subject   varchar(40)        not null,
+    memo      text               not null,
+    image     blob,
+    create_at timestamp,
+    update_at timestamp,
+    foreign key (user_id) references users (id)
+);
+
+insert into users(name,password,created_at,updated_at) values ('テスト','0000',now(),now());
