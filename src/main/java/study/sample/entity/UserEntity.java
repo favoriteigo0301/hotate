19f755fc2d65c07c4fd758b8d2cc4350ef0a81c0
favoriteigo0301 <smalllogical@gmail.com>
2021-01-23 13:09:24 +0900

    #10対応

---
+++ src/main/java/study/sample/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package study.sample.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
