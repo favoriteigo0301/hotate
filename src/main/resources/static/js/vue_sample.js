8d1cd4080da65b3478d698066269839420214fe7
favoriteigo0301 <smalllogical@gmail.com>
2021-01-30 16:28:17 +0900

    #27対応

--- src/main/resources/static/js/vue_sample.js
+++ src/main/resources/static/js/vue_sample.js
@@ -7,6 +7,7 @@ new Vue({
         titleErrorMessage: '',
         categoryErrorMessage:'',
         detailErrorMessage:'',
+        outputCategories:[],
         checkFlg: true
     },
     methods: {
@@ -27,6 +28,16 @@ new Vue({
                 // form送信をキャンセルする
                 event.preventDefault()
             }
+        }, addCategory:function (event) {
+            //alert("追加ボタン押されたよ")
+            if (this.category) {
+                this.outputCategories.push(this.category)
+                this.category = ''
+            } else {
+                this.categoryErrorMessage = "タグ名を入力してください"
+                this.checkFlg = false
+            }
         }
+
     }
 })
