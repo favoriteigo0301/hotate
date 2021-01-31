f0afb001caa15f3277b044762337b5b3d98445ce
favoriteigo0301 <smalllogical@gmail.com>
2021-01-31 10:06:27 +0900

    カテゴリーテーブル追加

--- src/main/resources/db/migration/V1__create_sample.sql
+++ src/main/resources/db/migration/V1__create_sample.sql
@@ -11,6 +11,7 @@ create table sample_memo
 (
     id        bigint primary key not null auto_increment,
     user_id   bigint not null,
+    category_ids varchar(100),
     subject   varchar(40)        not null,
     memo      text               not null,
     image     mediumblob,
@@ -18,6 +19,15 @@ create table sample_memo
     created_at timestamp,
     updated_at timestamp,
     foreign key (user_id) references users (id)
+);
+
+create table sample_category
+(
+    id bigint primary key not null  auto_increment,
+    name varchar(40) not null,
+    total mediumint,
+    created_at timestamp,
+    updated_at timestamp
 );
 
 insert into users(name,password,created_at,updated_at) values ('テスト','0000',now(),now());
